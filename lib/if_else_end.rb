14375a49a9359e047df215531e516ca99858df80
Thomas Garayua <tgarayua@gmail.com>
2020-09-10 04:14:29 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,9 @@
 # Write your solution here
-current_time = time.now 
 
+current_time = Time.now 
 current_time = current_time.to_i 
 
-if current_time % 2 == 0
+if current_time.even?
   puts "Even!"
 else
   puts "Odd!"
